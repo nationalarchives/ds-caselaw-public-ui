ece552a53d728c76f3c889f1b398b2d9aa51d20c
Laura Porter <laura@dxw.com>
2022-01-27 16:30:25 +0000

    Basic view and template to show the title of a piece of test data

--- judgements/urls.py
+++ judgements/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<str:judgement_uri>/", views.detail, name="detail"),
 ]
--- judgements/views.py
+++ judgements/views.py
@@ -1,5 +1,8 @@
+import requests
+import xmltodict
 from django.http import HttpResponse
 from django.template import loader
+from requests.auth import HTTPBasicAuth
 
 from .models import Judgement
 
@@ -8,4 +11,15 @@ def index(request):
     judgements = Judgement.objects.all
     context = {"judgement_list": judgements}
     template = loader.get_template("judgement/index.html")
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, judgement_uri):
+    response = requests.get(
+        "http://localhost:8011/LATEST/documents/?uri=/" + judgement_uri + ".xml",
+        auth=HTTPBasicAuth("rest-reader", "x"),
+    )
+    data = xmltodict.parse(response.text)
+    context = {"xml": data}
+    template = loader.get_template("judgement/detail.html")
     return HttpResponse(template.render(context, request))
