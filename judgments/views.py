16c5d3166f9e0c5abf07105679d29a1d4f0146ce
Laura Porter <laura@dxw.com>
2022-02-07 16:53:56 +0000

    Refactor API call in view into an API client class

--- judgments/views.py
+++ judgments/views.py
@@ -1,27 +1,19 @@
-import requests
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse
 from django.template import loader
-from requests.auth import HTTPBasicAuth
 
-from config.settings.base import env
+from judgments.api_client import ApiClient
 
 
 def detail(request, judgment_uri):
     if judgment_uri.endswith("/"):
         judgment_uri = judgment_uri[:-1]
 
-    response = requests.get(
-        "http://"
-        + env("MARKLOGIC_HOST")
-        + ":8011/LATEST/documents/?uri=/"
-        + judgment_uri
-        + ".xml",
-        auth=HTTPBasicAuth("admin", "admin"),
-    )
+    response = ApiClient.get(judgment_uri)
 
     if response.status_code != 200:
-        return HttpResponseNotFound("That judgment was not found")
+        context = {"content": "That judgment was not found"}
     else:
         context = {"xml": response.text}
-        template = loader.get_template("judgment/detail.html")
-        return HttpResponse(template.render(context, request))
+
+    template = loader.get_template("judgment/detail.html")
+    return HttpResponse(template.render(context, request))
