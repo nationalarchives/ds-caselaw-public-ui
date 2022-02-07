c88bb80ec7732066d10abd8f3803ada1fd7c0a8f
Andy Babic <andy.babic@torchbox.com>
2022-02-07 19:55:26 +0000

    Avoid ALLOWED_HOSTS restrictions completely locally

--- config/settings/local.py
+++ config/settings/local.py
@@ -7,7 +7,7 @@ from .base import env
 DEBUG = True
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["*"]
 
 # CACHES
 # ------------------------------------------------------------------------------
