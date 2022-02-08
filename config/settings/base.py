c9f6b6975c69eba17bd4b20fb89d5bc297749d10
Andy Babic <andy.babic@torchbox.com>
2022-02-08 12:03:03 +0000

    Add a default DATABASE_URL to settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -43,7 +43,7 @@ LOCALE_PATHS = [str(ROOT_DIR / "locale")]
 # DATABASES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#databases
-DATABASES = {"default": env.db("DATABASE_URL")}
+DATABASES = {"default": env.db("DATABASE_URL", default="sqlite://")}
 DATABASES["default"]["ATOMIC_REQUESTS"] = True
 # https://docs.djangoproject.com/en/stable/ref/settings/#std:setting-DEFAULT_AUTO_FIELD
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
