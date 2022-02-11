933e2544ade438d5419094115ef3c7649fa020ed
Laura Porter <laura@dxw.com>
2022-02-11 14:33:59 +0000

    Add URL paths for Open Justice Licence and Terms of Use
    These pages are empty for now, but add URL paths so that other pages have
    something to refer to

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,16 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
+    path(
+        "open-justice-licence",
+        TemplateView.as_view(template_name="pages/open_justice_licence.html"),
+        name="open_justice_licence",
+    ),
+    path(
+        "terms-of-use",
+        TemplateView.as_view(template_name="pages/terms_of_use.html"),
+        name="terms_of_use",
+    ),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # Your stuff: custom urls includes go here
