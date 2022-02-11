6fd5bba02bc8e5c5d3ffe9fcb640e508b9f4ebb4
Laura Porter <laura@dxw.com>
2022-02-11 14:24:39 +0000

    Remove About page

--- config/urls.py
+++ config/urls.py
@@ -7,9 +7,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    ),
     # Django Admin, use {% url 'admin:index' %}
     path(settings.ADMIN_URL, admin.site.urls),
     # Your stuff: custom urls includes go here
