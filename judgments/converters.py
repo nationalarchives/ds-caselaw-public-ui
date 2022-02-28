4039e789ee1cb642bc156cfb2f6063316cf990ca
Lewis Dale <lewis.dale@fortacode.co.uk>
2022-02-28 14:24:15 +0000

    Basic route outline for browsing by court and year, tests for custom url converters

---
+++ judgments/converters.py
@@ -0,0 +1,41 @@
+import datetime
+
+class YearConverter:
+    regex = "[0-9]{4}"
+
+    def to_python(self, value):
+        return int(value)
+
+    def to_url(self, value):
+        return '%04d' % value
+
+class DateConverter:
+    regex = "([0-9]{4})-([0-9]{2})-([0-9]{2})"
+
+    def to_python(self, value):
+        return datetime.datetime.strptime(value, "%Y-%m-%d")
+
+    def to_url(self, value):
+        if value is None:
+            raise ValueError
+
+        return value.strftime("%Y-%m-%d")
+
+class CourtConverter:
+    regex = "ewhc|uksc|ukpc|ewca|ewcop|ewfc"
+
+    def to_python(self, value):
+        return value
+
+    def to_url(self, value):
+        return value
+
+class SubdivisionConverter:
+    regex = "civ|crim|admin|admlty|ch|comm|costs|fam|ipec|mercantile|pat|qb|tcc"
+
+    def to_python(self, value):
+        return value
+
+    def to_url(self, value):
+        return value
+
--- judgments/urls.py
+++ judgments/urls.py
@@ -1,11 +1,23 @@
-from django.urls import path, re_path
+from django.urls import path, re_path, register_converter
 
-from . import views
+from . import converters, views
+
+register_converter(converters.YearConverter, "yyyy")
+register_converter(converters.DateConverter, "date")
+register_converter(converters.CourtConverter, "court")
+register_converter(converters.SubdivisionConverter, "subdivision")
 
 urlpatterns = [
+    path("<court:court>", views.browse, name="browse"),
+    path("<yyyy:year>", views.browse, name="browse"),
+    path("<court:court>/<yyyy:year>", views.browse, name="browse"),
+    path("<court:court>/<subdivision:subdivision>", views.browse, name="browse"),
+    path("<court:court>/<subdivision:subdivision>/<yyyy:year>", views.browse, name="browse"),
+
     re_path("(?P<judgment_uri>.*/.*/.*)/data.xml", views.detail_xml, name="detail"),
     re_path("(?P<judgment_uri>.*/.*/.*)/data.html", views.detail, name="detail"),
     re_path("(?P<judgment_uri>.*/.*/.*)", views.detail, name="detail"),
+
     path("judgments/results", views.results, name="results"),
     path("", views.index, name="home"),
 ]
