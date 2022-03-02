4ea8d6b7f72352e3841dea3085edd5e83e5d1aca
Lewis Dale <lewis.dale@fortacode.co.uk>
2022-03-02 12:00:01 +0000

    (Chore) remove unused import from config/urls.py

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
 
 from . import views
 
