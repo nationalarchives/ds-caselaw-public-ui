2289f7df483256c2e98bbb562f763b38ceb52246
Lewis Dale <lewis.dale@fortacode.co.uk>
2022-03-08 15:19:13 +0000

    (fix) Wrap debug toolbar config inside an if-block to prevent it being added when debug=False

--- config/settings/local.py
+++ config/settings/local.py
@@ -41,15 +41,16 @@ def show_debug_toolbar(request):
 
 
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
-INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
-# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
-MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
-# https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
-DEBUG_TOOLBAR_CONFIG = {
-    "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
-    "SHOW_TEMPLATE_CONTEXT": True,
-    "SHOW_TOOLBAR_CALLBACK": show_debug_toolbar,
-}
+if DEBUG is True:
+    INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
+    # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
+    MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
+    # https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
+    DEBUG_TOOLBAR_CONFIG = {
+        "DISABLE_PANELS": ["debug_toolbar.panels.redirects.RedirectsPanel"],
+        "SHOW_TEMPLATE_CONTEXT": True,
+        "SHOW_TOOLBAR_CALLBACK": show_debug_toolbar,
+    }
 
 # django-extensions
 # ------------------------------------------------------------------------------
