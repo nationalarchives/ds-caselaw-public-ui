a33ef41392cc1409eea30d9e4ad66e41fc76f559
Lewis Dale <lewis.dale@fortacode.co.uk>
2022-03-08 15:21:24 +0000

    (Chore) remove redundant is True

--- config/settings/local.py
+++ config/settings/local.py
@@ -41,7 +41,7 @@ def show_debug_toolbar(request):
 
 
 # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
-if DEBUG is True:
+if DEBUG:
     INSTALLED_APPS += ["debug_toolbar"]  # noqa F405
     # https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
     MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]  # noqa F405
