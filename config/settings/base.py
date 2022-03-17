5818d6468177fdda31cac41fbde7196d8c448399
Laura Porter <laura@dxw.com>
2022-03-17 17:19:44 +0000

    Add Rollbar
    The ROLLBAR_ACCESS_TOKEN is stored in 1password
    
    Do not send errors to Rollbar in development. When we have staging up and
    running we can add a staging Rollbar environment.

--- config/settings/base.py
+++ config/settings/base.py
@@ -136,6 +136,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.common.BrokenLinkEmailsMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "rollbar.contrib.django.middleware.RollbarNotifierMiddleware",
 ]
 
 # STATIC
