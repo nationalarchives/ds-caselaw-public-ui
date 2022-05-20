795ecdaf1119cc4fd9f657b8af570cebcbe52c98
Laura Porter <laura@dxw.com>
2022-05-20 11:26:45 +0100

    Improve the query string passed to the paginator & "order by" template
    Previously, we were always removing the `order` query string in the context, so
    that when this query string was used by the "order by" fragment, it did not
    already contain
    an `order` parameter. However, this had the side effect of removing the `order`
    parameter from the paginator, so only the first page of results was ordered
    correctly (subsequent pages had default ordering).
    
    Now, we have added a template filter to remove a named param from the query
    string, and can use this filter to remove the `order` param in the template. This
    does mean that we need to pass the `query_params` in the context for the
    `results` view.

--- judgments/templatetags/query_filters.py
+++ judgments/templatetags/query_filters.py
@@ -8,3 +8,10 @@ def remove_query(query_params, key):
     params = dict(query_params)
     params[key] = None
     return "&".join([f'{key}={params[key] or ""}' for key in params])
+
+
+@register.filter
+def remove_query_key(query_params, key):
+    params = dict(query_params)
+    params.pop(key)
+    return "&".join([f'{key}={params[key] or ""}' for key in params])
