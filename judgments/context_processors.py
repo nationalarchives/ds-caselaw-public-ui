2518c04abece2e69e319e7c234f11744194b4141
David McKee <dragon@dxw.com>
2022-07-22 11:28:26 +0100

    Use context processor instead of middleware for cookie consent
    With many, many thanks to Abdul Aziz Barkat for their clear
    explanation of why this is happening under the hood and the
    right approach to take:
    
    https://stackoverflow.com/questions/73070115/why-is-the-middleware-not-processed-on-404-page-in-django/73070884#73070884

---
+++ judgments/context_processors.py
@@ -0,0 +1,19 @@
+import json
+from urllib.parse import unquote
+
+
+def cookie_consent(request):
+    showGTM = False
+    dontShowCookieNotice = False
+    cookie_policy = request.COOKIES.get("cookies_policy", None)
+    dont_show_cookie_notice = request.COOKIES.get("dontShowCookieNotice", None)
+
+    if cookie_policy:
+        decoder = json.JSONDecoder()
+        decoded = decoder.decode(unquote(cookie_policy))
+        showGTM = decoded["usage"] or False
+
+    if dont_show_cookie_notice == "true":
+        dontShowCookieNotice = True
+
+    return {"showGTM": showGTM, "dontShowCookieNotice": dontShowCookieNotice}
