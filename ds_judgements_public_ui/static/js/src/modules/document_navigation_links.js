cc79936501a59a0a514c6337eeeb38616d7b933c
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2022-09-26 18:09:09 +0200

    fix js so that nav links consistently hide and show

--- ds_judgements_public_ui/static/js/src/modules/document_navigation_links.js
+++ ds_judgements_public_ui/static/js/src/modules/document_navigation_links.js
@@ -17,38 +17,40 @@ const manageClass = (intersecting) => {
     const pageScrolls = $(".judgment-body").height() > $(window).height();
 
     if (intersecting && pageScrolls) {
-        backToTopLinkContainer.classList.add("show");
-    } else {
         backToTopLinkContainer.classList.remove("show");
+    } else {
+        backToTopLinkContainer.classList.add("show");
     }
 };
 
-let judgmentBody = document.querySelector(".judgment-body");
-const backToTopLinkContainer = document.getElementById(
-    "js-document-navigation-links-end"
-);
+$(() => {
+    const backToTopLinkContainer = document.getElementById(
+        "js-document-navigation-links-end"
+    );
 
-let skipToEndLinkContainer = document.getElementById(
-    "js-document-navigation-links-start"
-);
+    let skipToEndLinkContainer = document.getElementById(
+        "js-document-navigation-links-start"
+    );
 
-let footerBackLink = document.querySelector(".end-document-marker__top-link a");
+    let footerBackLink = document.querySelector(
+        ".end-document-marker__top-link a"
+    );
 
-if (footerBackLink) {
-    footerBackLink.classList.add("with-js");
-}
-
-if (backToTopLinkContainer) {
-    backToTopLinkContainer.classList.add("autohide");
-    if (skipToEndLinkContainer) {
-        $(skipToEndLinkContainer)
-            .children()
-            .each((_index, link) => {
-                backToTopLinkContainer.appendChild(link);
-            });
+    if (footerBackLink) {
+        footerBackLink.classList.add("with-js");
     }
-}
 
-if (judgmentBody) {
-    createObserver(judgmentBody);
-}
+    if (backToTopLinkContainer) {
+        backToTopLinkContainer.classList.add("autohide");
+        if (skipToEndLinkContainer) {
+            $(skipToEndLinkContainer)
+                .children()
+                .each((_index, link) => {
+                    backToTopLinkContainer.appendChild(link);
+                });
+        }
+    }
+
+    createObserver(document.querySelector(".judgments-footer"));
+    createObserver(document.querySelector(".judgment-toolbar__container"));
+});
