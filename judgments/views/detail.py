8bab523347ab5e0d7954ccf0d4344d651e9d6bba
David McKee <david.mckee@dxw.com>
2023-08-02 11:42:06 +0100

    content_from_xml now value not function

--- judgments/views/detail.py
+++ judgments/views/detail.py
@@ -123,7 +123,7 @@ def detail(request, document_uri):
 def detail_xml(_request, document_uri):
     document = get_published_document_by_uri(document_uri)
 
-    document_xml = document.content_as_xml()
+    document_xml = document.content_as_xml
 
     response = HttpResponse(document_xml, content_type="application/xml")
     response["Content-Disposition"] = f"attachment; filename={document.uri}.xml"
