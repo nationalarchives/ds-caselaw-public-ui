2781c8642443ff35e08d4758d071ed36a319abe3
David McKee <david.mckee@dxw.com>
2023-08-18 17:28:10 +0100

    not quite good

--- judgments/tests/test_utils.py
+++ judgments/tests/test_utils.py
@@ -3,6 +3,9 @@ from unittest import mock
 
 from caselawclient.errors import DocumentNotFoundError
 
+from judgments.utils import get_document_by_uri
+from judgments.views.detail import NoNeutralCitationError
+
 
 class TestGetDocumentByUri(unittest.TestCase):
     @mock.patch("judgments.utils.MarklogicApiClient")
@@ -20,10 +23,10 @@ class TestGetDocumentByUri(unittest.TestCase):
         with self.assertRaises(DocumentNotFoundError):
             get_document_by_uri("nonexistent_uri")
 
-  @mock.patch('judgments.utils.MarklogicApiClient')
-  def test_get_nonexistent_document(self, mock_api_client):
-    document = mock.Mock()
-    document.best_human_identifier=None
-    mock_api_client.return_value.get_document_by_uri.return_value = document
-    with self.assertRaises(NoNeutralCitationError):
-      get_document_by_uri('nonexistent_uri')
+    @mock.patch("judgments.utils.MarklogicApiClient")
+    def test_get_document_not_judgment_or_press_summary(self, mock_api_client):
+        document = mock.Mock()
+        document.best_human_identifier = None
+        mock_api_client.return_value.get_document_by_uri.return_value = document
+        with self.assertRaises(NoNeutralCitationError):
+            get_document_by_uri("nonexistent_uri")
