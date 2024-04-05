6e770f7e9efcf66d0b4569e864b284459978a215
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-04-05 11:17:39 +0200

    Lay out the pages and fields for the form
    As in v0.8 of the field def spreadsheet, with reference to:
    - the prototype at https://o88kd3.axshare.com/form_b_-_part_1.html
    - the Miro board at https://miro.com/app/board/uXjVNlAQ7po=/?moveToWidget=3458764583080457330&cot=14

--- webpack.config.js
+++ webpack.config.js
@@ -8,8 +8,6 @@ module.exports = {
         cookie_consent:
             "./ds_judgements_public_ui/static/js/cookie_consent/src/ds-cookie-consent.js",
         gtm_script: "./ds_judgements_public_ui/static/js/src/gtm_script.js",
-        "transactional_licence_form/app":
-            "./ds_judgements_public_ui/static/js/src/transactional_licence_form/app.js",
     },
     output: {
         filename: "[name].js",
