6a4df250fc52e0dde152e7572f32116895c5fbd3
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-04-10 13:33:17 +0200

    Fix review / submit steps.
    The review step can't happen in the 'done' callback as the session storage of the form values is wiped on rendering, making it impossible to go back!
    
    Instead we make a 'dummy step' with a custom template, and extend the WizardView to pass all the existing form data through to the template on each step.

--- transactional_licence_form/views.py
+++ transactional_licence_form/views.py
@@ -4,14 +4,21 @@ from formtools.wizard.views import NamedUrlSessionWizardView
 
 from . import forms
 
+TEMPLATE_OVERRIDES = {"review": "review.html"}
+
 
 class FormWizardView(NamedUrlSessionWizardView):
-    template_name = "form.html"
+
+    def get_template_names(self):
+        return [TEMPLATE_OVERRIDES.get(self.steps.current, "form.html")]
+
+    def get_context_data(self, form, **kwargs):
+        context = super(FormWizardView, self).get_context_data(form)
+        context["all_data"] = self.get_all_cleaned_data()
+        return context
 
     def done(self, form_list, **kwargs):
-        return render(self.request, "review.html", {"forms": form_list})
-
-    template_name = "form.html"
+        return render(self.request, "submitted.html")
 
 
 class StartView(TemplateView):
@@ -22,5 +29,5 @@ def wizard_view(url_name):
     return FormWizardView.as_view(
         forms.FORMS,
         url_name=url_name,
-        done_step_name="review",
+        done_step_name="submitted",
     )
