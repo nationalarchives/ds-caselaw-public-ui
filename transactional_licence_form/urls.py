162c529b8c8ea74fbf6c96ef4b5a900850361fee
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-04-11 15:45:20 +0200

    add stubs for confirmation step

--- transactional_licence_form/urls.py
+++ transactional_licence_form/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, re_path
 
-from .views import StartView1, StartView2, StartView3, wizard_view
+from .views import (
+    ConfirmationView,
+    EarlyExitView,
+    StartView1,
+    StartView2,
+    StartView3,
+    wizard_view,
+)
 
 form_name = "transactional-licence-form-steps"
 form_view = wizard_view("%s-step" % form_name)
@@ -9,6 +16,16 @@ urlpatterns = [
     path("", StartView1.as_view(), name="transactional-licence-form"),
     path("/page-2", StartView2.as_view(), name="transactional-licence-form-page-2"),
     path("/page-3", StartView3.as_view(), name="transactional-licence-form-page-3"),
+    path(
+        "/confirmation",
+        ConfirmationView.as_view(),
+        name="transactional-licence-form-confirmation",
+    ),
+    path(
+        "/early-exit",
+        EarlyExitView.as_view(),
+        name="transactional-licence-form-early-exit",
+    ),
     re_path(
         r"^/steps/(?P<step>.+)/$",
         form_view,
