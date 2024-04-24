348dd52837ead8f93c24980bd0a9189901ecb2f6
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-04-24 11:53:37 +0200

    correct form button labels

--- transactional_licence_form/templatetags/transactional_licence_utils.py
+++ transactional_licence_form/templatetags/transactional_licence_utils.py
@@ -1,3 +1,14 @@
 from django import template
 
 register = template.Library()
+
+
+@register.filter
+def submit_label_for_step(step):
+    match step:
+        case "nine-principles":
+            return "Review your answers"
+        case "review":
+            return "Submit your application"
+        case _:
+            return "Next"
