5ad00c70b98ac53614222b18c5d7f8d23fec0e65
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-04-24 13:05:55 +0200

    TLF: territory autocomplete

--- ds_judgements_public_ui/static/js/src/app.js
+++ ds_judgements_public_ui/static/js/src/app.js
@@ -2,4 +2,5 @@ import { initAll } from "govuk-frontend";
 import "./modules/manage_filters";
 import "./modules/manage_aria_buttons";
 import "./modules/document_navigation_links";
+import "./modules/location_picker";
 initAll();
