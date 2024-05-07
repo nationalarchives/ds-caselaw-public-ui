56da7681375a1f752bab1bc8998e54730d109aa7
Donna-H <donna.hussey@nationalarchives.gov.uk>
2024-05-07 18:02:10 +0100

    update confirmation.html add button to temporary downlage page

--- transactional_licence_form/urls.py
+++ transactional_licence_form/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, re_path
 
-from .views import ConfirmationView, StartView1, StartView2, StartView3, DownloadView, wizard_view
+from .views import (
+    ConfirmationView,
+    DownloadView,
+    StartView1,
+    StartView2,
+    StartView3,
+    wizard_view,
+)
 
 form_name = "transactional-licence-form-steps"
 form_view = wizard_view("%s-step" % form_name)
@@ -9,7 +16,11 @@ urlpatterns = [
     path("", StartView1.as_view(), name="transactional-licence-form"),
     path("/page-2", StartView2.as_view(), name="transactional-licence-form-page-2"),
     path("/page-3", StartView3.as_view(), name="transactional-licence-form-page-3"),
-    path("/download-application", DownloadView.as_view(), name="application-download-page"),
+    path(
+        "/download-application",
+        DownloadView.as_view(),
+        name="application-download-page",
+    ),
     path(
         "/confirmation",
         ConfirmationView.as_view(),
