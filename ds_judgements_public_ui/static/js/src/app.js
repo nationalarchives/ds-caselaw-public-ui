aa664f18d9a9aff4af1c6b6a4846198f6c85d186
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-05-07 10:32:44 +0200

    hide alternative contact form when no alternative contact

--- ds_judgements_public_ui/static/js/src/app.js
+++ ds_judgements_public_ui/static/js/src/app.js
@@ -3,4 +3,5 @@ import "./modules/manage_filters";
 import "./modules/manage_aria_buttons";
 import "./modules/document_navigation_links";
 import "./modules/location_picker";
+import "./modules/transactional_licence_form";
 initAll();
