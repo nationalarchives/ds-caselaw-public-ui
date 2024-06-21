4b293a7605f48e563efa9d95f24d49e905656f9b
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2024-06-21 14:07:21 +0200

    basic infrastructure for playwright e2 tests
    ignore e2e tests in normal pytest run
    
    can't use fab on github actions
    
    github action for end to end testes needs the base url to be passed
    
    actually start the django server in CI
    
    test a content-only page as a first tracer-bullet as the homepage requires marklogic

---
+++ e2e_tests/test_about_page.py
@@ -0,0 +1,14 @@
+from playwright.sync_api import Page, expect
+
+
+def test_about_page(page: Page):
+    """
+    A minimum-viable-integration test of a page
+    that doesn't talk to marklogic or any other
+    external services.
+    """
+    page.goto("/about-this-service")
+
+    expect(page).to_have_title(
+        "About this service - Find case law - The National Archives"
+    )
