8a192d97ba2a77654effec88579e93a74ae83519
James Harries <dxw@pancakeuprising.com>
2024-06-24 12:09:25 +0100

    FCL-107 | WIP - use templatetags to style active navigation links

---
+++ judgments/templatetags/navigation_tags.py
@@ -0,0 +1,14 @@
+from django import template
+
+from django.urls import resolve
+
+register = template.Library()
+
+@register.simple_tag(takes_context=True)
+def navigation_item_class(context, path):
+    request = context["request"]
+
+    if resolve(request.path_info).url_name == path:
+        return "govuk-header__navigation-item govuk-header__navigation-item--active"
+
+    return "govuk-header__navigation-item"
