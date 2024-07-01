d2c66e96994f750dd837e4067ea7604d106e66ab
Donna-H <donna.hussey@nationalarchives.gov.uk>
2024-07-01 12:13:48 +0100

    update header on base for pages with breadcrumbs

--- judgments/templatetags/navigation_tags.py
+++ judgments/templatetags/navigation_tags.py
@@ -1,8 +1,8 @@
 from django import template
-
 from django.urls import resolve
 
 register = template.Library()
+
 
 @register.simple_tag(takes_context=True)
 def navigation_item_class(context, path):
