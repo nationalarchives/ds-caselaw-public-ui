04fd6cefd03a4757b15947533f5e97485257c9c0
James Harries <dxw@pancakeuprising.com>
2024-07-05 17:22:15 +0100

    FCL-150 | add spacing and typography to style guide

--- config/views.py
+++ config/views.py
@@ -100,6 +100,37 @@ class StyleGuide(TemplateViewWithContext):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["feedback_survey_type"] = "support"
+        context["menu_items"] = [
+            {
+                "label": "Components",
+                "href": "#components",
+                "children": [
+                    {"label": "Character count", "href": "#character-count"},
+                    {"label": "Checkboxes", "href": "#checkboxes"},
+                    {"label": "Details box", "href": "#details-box"},
+                    {"label": "Important information box", "href": "#important-information-box"},
+                    {"label": "Notification banners", "href": "#notification-banners"},
+                    {"label": "Radios", "href": "#radios"},
+                    {"label": "Summary card", "href": "#summary-card"},
+                    {"label": "Text input", "href": "#text-input"},
+                ],
+            },
+            {
+                "label": "Spacing",
+                "href": "#spacing",
+            },
+            {
+                "label": "Typography",
+                "href": "#typography",
+                "children": [
+                    {"label": "Font family", "href": "#font-family"},
+                    {"label": "Font sizes", "href": "#font-sizes"},
+                    {"label": "Font weights", "href": "#font-weights"},
+                    {"label": "Headings", "href": "#headings"},
+                    {"label": "Line heights", "href": "#line-heights"},
+                ],
+            },
+        ]
         return context
 
 
