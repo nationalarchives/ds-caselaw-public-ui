0644a30d1c6cdc11204b4f6052e62e9c13c4321b
James Harries <dxw@pancakeuprising.com>
2024-07-11 15:11:07 +0100

    FCL-147 | Improve HTML assertion helper

--- judgments/tests/utils/assertions.py
+++ judgments/tests/utils/assertions.py
@@ -2,22 +2,55 @@
 Custom assertion helpers for tests.
 """
 
-
-def assert_contains_html(response, html):
-    """
-    Asserts that the given HTML is contained within the response content.
-
-    Raises:
-        AssertionError: If the HTML is not found in the response content.
-    """
-    assert html.replace(" ", "").replace("\n", "") in response.content.decode().replace(" ", "").replace("\n", "")
+from lxml import html
 
 
-def assert_not_contains_html(response, html):
-    """
-    Asserts that the given HTML is not contained within the response content.
+def parse_html(html_string):
+    return html.fromstring(html_string)
 
-    Raises:
-        AssertionError: If the HTML is found in the response content.
-    """
-    assert html.replace(" ", "").replace("\n", "") not in response.content.decode().replace(" ", "").replace("\n", "")
+
+def parse_html_fragment(html_string):
+    return html.fragments_fromstring(html_string)
+
+
+def normalise_text(text):
+    return " ".join(text.split())
+
+
+def elements_match(element_1, element_2):
+    if element_1.tag != element_2.tag:
+        return False
+
+    if (normalise_text(element_1.text or "")) != normalise_text(element_2.text or ""):
+        return False
+
+    if element_1.attrib != element_2.attrib:
+        return False
+
+    children_1 = list(element_1)
+    children_2 = list(element_2)
+
+    if len(children_1) != len(children_2):
+        return False
+
+    return all(elements_match(c1, c2) for c1, c2 in zip(children_1, children_2))
+
+
+def is_contained(container_tree, contained_tree):
+    return all(
+        any(elements_match(element, fragment) for element in container_tree.iter()) for fragment in contained_tree
+    )
+
+
+def response_contains_html(response, contained_html):
+    container_tree = parse_html(response.content.decode())
+    contained_tree = parse_html_fragment(contained_html)
+    return is_contained(container_tree, contained_tree)
+
+
+def assert_contains_html(response, contained_html):
+    assert response_contains_html(response, contained_html)
+
+
+def assert_not_contains_html(response, contained_html):
+    assert not response_contains_html(response, contained_html)
