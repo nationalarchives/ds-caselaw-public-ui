19641987fc90b8325a804d198f4fa82e4789dea6
James Harries <dxw@pancakeuprising.com>
2024-08-30 04:32:30 +0100

    FCL-246 | add all advanced search query params to judgment url on search and on breadcrumbs

--- judgments/views/detail.py
+++ judgments/views/detail.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import urllib
 from typing import Any
 
 import requests
@@ -14,6 +15,7 @@ from django.urls import reverse
 from django.views.generic import TemplateView
 from django_weasyprint import WeasyTemplateResponseMixin
 
+from judgments.forms import AdvancedSearchForm
 from judgments.models.document_pdf import DocumentPdf
 from judgments.utils import (
     get_document_by_uri,
@@ -129,10 +131,14 @@ def detail(request, document_uri):
     context["pdf_size"] = f" ({filesizeformat(pdf.size)})" if pdf.size else " (unknown size)"
     context["pdf_uri"] = pdf.uri
 
+    form: AdvancedSearchForm = AdvancedSearchForm(request.GET)
+
     breadcrumbs = []
 
-    if query:
-        breadcrumbs.append({"url": "/judgments/search?query=" + query, "text": f'Search results for "{query}"'})
+    if query and form.is_valid():
+        query_param_string = urllib.parse.urlencode(form.cleaned_data, doseq=True)
+
+        breadcrumbs.append({"url": "/judgments/search?" + query_param_string, "text": f'Search results for "{query}"'})
 
     if document.document_noun == "press summary" and context["linked_document_uri"]:
         breadcrumbs.append(
