58a474fad0084c057677f83d06d465f9bc3b562a
David McKee <david.mckee@dxw.com>
2024-09-02 11:14:24 +0100

    EWCR not EWCRC

--- judgments/converters.py
+++ judgments/converters.py
@@ -25,7 +25,7 @@ class DateConverter:
 
 
 class CourtConverter:
-    regex = "ewhc|uksc|ukpc|ewca|ewcop|ewfc|ukut|eat|ukftt|ukait|ukiptrib|ukcc|ukcrc"
+    regex = "ewhc|uksc|ukpc|ewca|ewcop|ewfc|ukut|eat|ukftt|ukait|ukiptrib|ukcc|ukcr"
 
     def to_python(self, value):
         return value
