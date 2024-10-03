f1e1ee01dafbb6ff8deccd85ce87b25c08c2dd73
Nick Jackson <nick@dxw.com>
2024-10-03 17:31:56 +0100

    feat(views): TemplateViewWithContext can now turn named paths into canonical URLs
    This simplifies adding canonical URLs to the context of views which derive from this class

--- config/views/template_view_with_context.py
+++ config/views/template_view_with_context.py
@@ -1,10 +1,16 @@
+from typing import Optional
+
+from django.urls import reverse
 from django.views.generic import TemplateView
 
 
 class TemplateViewWithContext(TemplateView):
-    page_title = None
+    page_title: Optional[str] = None
+    page_canonical_url_name: Optional[str] = None
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["page_title"] = self.page_title
+        if self.page_canonical_url_name:
+            context["page_canonical_url"] = self.request.build_absolute_uri(reverse(self.page_canonical_url_name))
         return context
