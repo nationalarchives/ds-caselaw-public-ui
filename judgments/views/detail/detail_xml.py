a6d96a5d9186f6ff733a0cf85b6dc39bab8429fb
James Harries <dxw@pancakeuprising.com>
2024-10-08 17:10:13 +0100

    FCL-163 | remove unnecessary log

--- judgments/views/detail/detail_xml.py
+++ judgments/views/detail/detail_xml.py
@@ -1,15 +1,8 @@
-import logging
-import os
-
 from django.http import HttpResponse
 
 from judgments.utils import (
     get_published_document_by_uri,
 )
-
-# suppress weasyprint log spam
-if os.environ.get("SHOW_WEASYPRINT_LOGS") != "True":
-    logging.getLogger("weasyprint").handlers = []
 
 
 def detail_xml(_request, document_uri):
