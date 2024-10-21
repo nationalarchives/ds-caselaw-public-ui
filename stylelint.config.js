b4f5fb6ff0c1a58df29a715c1d711aae7817016e
James Harries <dxw@pancakeuprising.com>
2024-10-21 21:13:06 +0100

    FCL-223 | use ds caselaw frontend stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,17 +1,3 @@
 module.exports = {
-    extends: [
-        "stylelint-config-standard-scss",
-        "stylelint-config-clean-order/error",
-    ],
-    rules: {
-        "selector-class-pattern": [
-            "^[a-z]([-]?[a-z0-9]+)*(__[a-z0-9]([-]?[a-z0-9]+)*)?(--[a-z0-9]([-]?[a-z0-9]+)*)?$",
-            {
-                resolveNestedSelectors: true,
-                message: function expected(selectorValue) {
-                    return `Expected class selector "${selectorValue}" to match BEM CSS pattern.`;
-                },
-            },
-        ],
-    },
+    extends: ["@nationalarchives/ds-caselaw-frontend/stylelint.config.js"],
 };
