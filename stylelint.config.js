0b835a4e858628c32d658c67ac2167ca7731348e
David McKee <david.mckee@dxw.com>
2024-10-31 14:12:04 +0000

    Ignore failing stylelint rules

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,18 @@
 module.exports = {
     extends: ["@nationalarchives/ds-caselaw-frontend/stylelint.config.js"],
+    rules: {
+        "selector-id-pattern": null,
+        "no-duplicate-selectors": null,
+        "selector-class-pattern": null,
+        "scss/at-mixin-pattern": null,
+        "no-empty-source": null,
+        "number-max-precision": null,
+        "no-descending-specificity": null,
+        "font-family-no-missing-generic-family-keyword": null,
+        "scss/no-global-function-names": null,
+        "block-no-empty": null,
+        "scss/operator-no-newline-after": null,
+        "scss/double-slash-comment-whitespace-inside": null,
+        "scss/dollar-variable-pattern": null,
+    },
 };
