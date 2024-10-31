691f51e10883cf82548f0a61986acfe9872fe2a7
David McKee <david.mckee@dxw.com>
2024-10-31 17:18:00 +0000

    catch resolver failure due to bad URLs

--- judgments/templatetags/navigation_tags.py
+++ judgments/templatetags/navigation_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from django.urls import resolve
+from django.urls import Resolver404, resolve
 
 register = template.Library()
 
@@ -8,7 +8,13 @@ register = template.Library()
 def navigation_item_class(context, path):
     request = context.get("request")
 
-    if request and resolve(request.path_info).url_name == path:
-        return "govuk-header__navigation-item govuk-header__navigation-item--active"
+    if request:
+        try:
+            request_path = resolve(request.path_info).url_name
+        except Resolver404:
+            request_path = None
+
+        if request and request_path and request_path == path:
+            return "govuk-header__navigation-item govuk-header__navigation-item--active"
 
     return "govuk-header__navigation-item"
