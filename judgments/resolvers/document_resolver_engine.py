a00f4896deadd8cc97276547ec4f0eebf5d29087
David McKee <david.mckee@dxw.com>
2024-12-10 16:45:42 +0000

    Resolve base page

--- judgments/resolvers/document_resolver_engine.py
+++ judgments/resolvers/document_resolver_engine.py
@@ -6,8 +6,53 @@ from django.http import Http404
 from django.http.request import HttpRequest
 from django.views.generic import View
 
+from judgments.utils.utils import api_client
 from judgments.views.detail import best_pdf, detail_html, detail_xml, generated_pdf
 from judgments.views.press_summaries import press_summaries
+
+
+class MarklogicInDocumentClothing(str):
+    def __new__(self, value):
+        return value.strip("/").replace(".xml", "")
+
+
+class IdentifierResolverEngine(View):
+    # TODO: make this not just a copy-paste
+
+    def dispatch(
+        self,
+        request: HttpRequest,
+        document_uri: str,
+        file_format: Optional[str] = None,
+        component: Optional[str] = None,
+    ):
+        fileformat_lookup = {
+            "data.pdf": best_pdf,
+            "generated.pdf": generated_pdf,
+            "data.xml": detail_xml,
+            "data.html": detail_html,
+        }
+        component_lookup = {
+            "press-summary": press_summaries,
+        }
+
+        resolutions = api_client.resolve_from_identifier(document_uri)
+        if len(resolutions) == 0:
+            msg = f"No resolutions for {document_uri}"
+            raise Http404(msg)
+        if len(resolutions) > 1:
+            msg = f"Too many resolutions for {document_uri}"
+            raise Http404(msg)
+
+        resolution_uri = MarklogicInDocumentClothing(resolutions[0].document_uri)
+
+        if file_format:
+            return fileformat_lookup[file_format](request, resolution_uri)
+
+        if component:
+            return component_lookup[component](request, resolution_uri)
+
+        return detail_html(request, resolution_uri)
 
 
 class DocumentResolverEngine(View):
