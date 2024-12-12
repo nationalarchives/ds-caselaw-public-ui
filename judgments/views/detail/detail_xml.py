828b4d4d3df9bcf33c11853de0f27a12468f1eeb
David McKee <david.mckee@dxw.com>
2024-12-12 15:51:09 +0000

    Fix XML download filename -- not ideal

--- judgments/views/detail/detail_xml.py
+++ judgments/views/detail/detail_xml.py
@@ -1,16 +1,21 @@
 from caselawclient.models.documents import DocumentURIString
 from django.http import HttpResponse
+from django.urls import resolve
 
 from judgments.utils import (
     get_published_document_by_uri,
 )
 
 
-def detail_xml(_request, document_uri: DocumentURIString) -> HttpResponse:
+def filename(request) -> str:
+    return resolve(request.path).captured_kwargs["document_uri"].replace("/", "_")
+
+
+def detail_xml(request, document_uri: DocumentURIString) -> HttpResponse:
     document = get_published_document_by_uri(document_uri)
 
     document_xml = document.body.content_as_xml
 
     response = HttpResponse(document_xml, content_type="application/xml")
-    response["Content-Disposition"] = f"attachment; filename={document.uri}.xml"
+    response["Content-Disposition"] = f"attachment; filename={filename(request)}.xml"
     return response
