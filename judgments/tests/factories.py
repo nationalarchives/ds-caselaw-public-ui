9680e85a9db024dd2847b1d7b84a18b6ba11227b
David McKee <david.mckee@dxw.com>
2024-12-18 11:18:49 +0000

    Fix tests.py -- it might be possible to remove half these tests as the resolver is doing the hard press-summary work

--- judgments/tests/factories.py
+++ judgments/tests/factories.py
@@ -1,3 +1,8 @@
+import uuid
+
+from caselawclient.identifier_resolution import IdentifierResolution, IdentifierResolutions
+from caselawclient.models.documents import DocumentURIString
+from caselawclient.xquery_type_dicts import MarkLogicDocumentURIString
 from factory.django import DjangoModelFactory
 
 from judgments.models import CourtDates
@@ -10,3 +15,20 @@ class CourtDateFactory(DjangoModelFactory):
     param = "uksc"
     start_year = 2001
     end_year = 2024
+
+
+class IdentifierResolutionsFactory:
+    @classmethod
+    def build(cls, identifier_uuid=None, uri="/ml_example_url.xml", slug="uksc/1979/999") -> IdentifierResolutions:
+        if not identifier_uuid:
+            identifier_uuid = str(uuid.uuid4())
+        return IdentifierResolutions(
+            [
+                IdentifierResolution(
+                    identifier_uuid=f"id-{uuid}",
+                    document_uri=MarkLogicDocumentURIString(uri),
+                    identifier_slug=DocumentURIString(slug),
+                    document_published=True,
+                )
+            ]
+        )
