282a55f711c4122eef62abd1cbe8096e7ee1237c
James Harries <dxw@pancakeuprising.com>
2024-12-18 16:34:09 +0000

    FCL-162 | encapsulate JS so it's only loaded on relevant pages

--- ds_judgements_public_ui/static/js/src/app.js
+++ ds_judgements_public_ui/static/js/src/app.js
@@ -1,9 +1,3 @@
 import { initAll } from "govuk-frontend";
-import "./modules/manage_filters";
-import "./modules/manage_aria_buttons";
-import "./modules/document_navigation_links";
-import "./modules/location_picker";
-import "./modules/transactional_licence_form";
-import "./modules/document_paragraph_tooltip_anchors";
-import "./modules/copy_link_tooltip";
+import "./global/manage_aria_buttons";
 initAll();
