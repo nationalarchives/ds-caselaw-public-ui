52c305944900622c772373c32bb95ac3b9efb460
Nick Jackson <nick@dxw.com>
2025-02-06 17:36:08 +0000

    WIP to make URL resolution work

--- judgments/resolvers/document_resolver_engine.py
+++ judgments/resolvers/document_resolver_engine.py
@@ -1,13 +1,11 @@
 from typing import Optional
 
-from caselawclient.models.documents import DocumentURIString
-from caselawclient.types import InvalidDocumentURIException
 from django.http import Http404
 from django.http.request import HttpRequest
 from django.views.generic import View
 
+from judgments.utils import api_client
 from judgments.views.detail import best_pdf, detail_html, detail_xml, generated_pdf
-from judgments.views.press_summaries import press_summaries
 
 
 class DocumentResolverEngine(View):
@@ -16,7 +14,6 @@ class DocumentResolverEngine(View):
         request: HttpRequest,
         document_uri: str,
         file_format: Optional[str] = None,
-        component: Optional[str] = None,
     ):
         fileformat_lookup = {
             "data.pdf": best_pdf,
@@ -24,19 +21,17 @@ class DocumentResolverEngine(View):
             "data.xml": detail_xml,
             "data.html": detail_html,
         }
-        component_lookup = {
-            "press-summary": press_summaries,
-        }
 
-        try:
-            document_uri = DocumentURIString(document_uri)
-        except InvalidDocumentURIException:
-            raise Http404("Document Resolver recieved an invalid DocumentURIString")
+        resolved_documents = api_client.resolve_from_identifier_slug(document_uri)
+
+        if not resolved_documents:
+            msg = f"Unable to find a matching document at {document_uri}"
+            raise Http404(msg)
+
+        if len(resolved_documents) > 1:
+            raise Exception("There's more than one document here! Uh-oh!")
 
         if file_format:
             return fileformat_lookup[file_format](request, document_uri)
 
-        if component:
-            return component_lookup[component](request, document_uri)
-
-        return detail_html(request, document_uri)
+        return detail_html(request, resolved_documents[0].document_uri.as_document_uri())
