572df3605d2fcd51630b306e1b651799304839de
David McKee <david.mckee@dxw.com>
2025-02-12 15:53:54 +0000

    wip: disambig

--- judgments/resolvers/document_resolver_engine.py
+++ judgments/resolvers/document_resolver_engine.py
@@ -7,6 +7,7 @@ from django.views.generic import View
 
 from judgments.utils import api_client
 from judgments.views.detail import best_pdf, detail_html, detail_xml, generated_pdf
+from judgments.views.disambiguation import DisambiguationView
 
 
 class DocumentResolverEngine(View):
@@ -23,16 +24,18 @@ class DocumentResolverEngine(View):
             "data.html": detail_html,
         }
 
-        resolved_documents = api_client.resolve_from_identifier_slug(DocumentIdentifierSlug(document_uri))
+        resolutions = api_client.resolve_from_identifier_slug(DocumentIdentifierSlug(document_uri))
 
-        if not resolved_documents:
+        if not resolutions:
             msg = f"Unable to find a matching document at {document_uri}"
             raise Http404(msg)
 
-        if len(resolved_documents) > 1:
-            raise Exception("There's more than one document here! Uh-oh!")
+        if len(resolutions) > 1:
+            return DisambiguationView.as_view()(
+                request, uri=document_uri, resolutions=resolutions, file_format=file_format
+            )
 
         if file_format:
             return fileformat_lookup[file_format](request, document_uri)
 
-        return detail_html(request, resolved_documents[0].document_uri.as_document_uri())
+        return detail_html(request, resolutions[0].document_uri.as_document_uri())
