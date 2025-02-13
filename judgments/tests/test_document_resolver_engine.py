271e5b52a072d0a960e6f9d068ae444cd45955b2
David McKee <david.mckee@dxw.com>
2025-02-13 18:07:34 +0000

    Test resolution engine

--- judgments/tests/test_document_resolver_engine.py
+++ judgments/tests/test_document_resolver_engine.py
@@ -1,9 +1,11 @@
 from unittest.mock import patch
 
+import pytest
+from django.http.response import Http404
 from django.test import RequestFactory
-from fixtures import TestCaseWithMockAPI
+from fixtures import TestCaseWithMockAPI, TestCaseWithMultipleResolutions, TestCaseWithNoResolutions
 
-from judgments.resolvers.document_resolver_engine import DocumentResolverEngine
+from judgments.resolvers.document_resolver_engine import DocumentResolverEngine, MultipleResolutionsError
 
 
 class TestDocumentResolverEngine(TestCaseWithMockAPI):
@@ -33,4 +35,29 @@ class TestDocumentResolverEngine(TestCaseWithMockAPI):
                 resolver_engine.setup(request)
                 resolver_engine.dispatch(request, document_uri, file_format=file_format)
 
-                view.assert_called_with(request, document_uri)
+                view.assert_called_with(request, "ml-" + document_uri)
+
+
+class TestDocumentResolverEngineWithNoResolutions(TestCaseWithNoResolutions):
+    def test_resolver_engine_with_no_resolutions(
+        self,
+    ):
+        document_uri = "ewhc/comm/2024/253"
+        request = RequestFactory().get(document_uri)
+        resolver_engine = DocumentResolverEngine()
+        resolver_engine.setup(request)
+        with pytest.raises(Http404):
+            resolver_engine.dispatch(request, document_uri)
+
+
+class TestDocumentResolverEngineWithMultipleResolutions(TestCaseWithMultipleResolutions):
+    def test_resolver_engine_with_multiple_resolutions(
+        self,
+    ):
+        """We will remove this test when we have a disambiguation system"""
+        document_uri = "ewhc/comm/2024/253"
+        request = RequestFactory().get(document_uri)
+        resolver_engine = DocumentResolverEngine()
+        resolver_engine.setup(request)
+        with pytest.raises(MultipleResolutionsError):
+            resolver_engine.dispatch(request, document_uri)
