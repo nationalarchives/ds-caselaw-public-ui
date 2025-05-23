e9753cde1b0a8efe0fce6248303eae23a702d1b2
Nick Jackson <nick@dxw.com>
2025-05-23 13:09:53 +0100

    Add path to download API schema

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,5 @@
+from django.urls import path
+from drf_spectacular.views import SpectacularAPIView
 from rest_framework import routers
 
 from .viewsets import bodies, identifier_types
@@ -6,4 +8,6 @@ router = routers.DefaultRouter(trailing_slash=False)
 router.register(r"bodies", bodies.BodiesViewSet, basename="body")
 router.register(r"identifier-types", identifier_types.IdentifierTypesViewSet, basename="identifier_type")
 
-urlpatterns = router.urls
+urlpatterns = router.urls + [
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+]
