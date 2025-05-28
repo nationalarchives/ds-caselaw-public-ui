b3ad5e932f74e89f4511eee7e4bdaf5fb88b4b55
Nick Jackson <nick@dxw.com>
2025-05-28 10:01:02 +0100

    WIP: Adding document view

---
+++ api/viewsets/documents.py
@@ -0,0 +1,54 @@
+from caselawclient.client_helpers.search_helpers import search_judgments_and_parse_response
+from caselawclient.search_parameters import SearchParameters
+from rest_framework import serializers, viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+from judgments.converters import DOCUMENT_URI_REGEX_PATTERN
+from judgments.utils import api_client, get_published_document_by_uri
+
+
+class DocumentSearchResultSerialiser(serializers.Serializer):
+    url = serializers.HyperlinkedIdentityField(view_name="api:document-detail", lookup_field="uri")
+    uri = serializers.CharField(read_only=True)
+    name = serializers.CharField(read_only=True)
+
+
+class SearchAsQuerysetFacade:
+    def __init__(self, search_params: SearchParameters):
+        self.search_params = search_params
+
+    def __len__(self):
+        return search_judgments_and_parse_response(api_client, self.search_params).total
+
+    def __getitem__(self, index):
+        if isinstance(index, slice):
+            self.search_params.page_size = index.stop - index.start
+            self.search_params.page = (index.start / self.search_params.page_size) + 1
+        else:
+            self.search_params.page_size = 1
+            self.search_params.page = index + 1
+
+        return search_judgments_and_parse_response(api_client, self.search_params).results
+
+
+class DocumentsViewPagination(PageNumberPagination):
+    page_size = 50
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+
+class DocumentsViewSet(viewsets.ReadOnlyModelViewSet):
+    pagination_class = DocumentsViewPagination
+    serializer_class = DocumentSearchResultSerialiser
+    lookup_field = "uri"
+    lookup_value_regex = DOCUMENT_URI_REGEX_PATTERN
+
+    def get_queryset(self):
+        search_params = SearchParameters(query=self.request.query_params.get("query"))
+
+        return SearchAsQuerysetFacade(search_params)
+
+    def retrieve(self, request, uri=None):
+        serializer = DocumentSearchResultSerialiser(get_published_document_by_uri(uri), context={"request": request})
+        return Response(serializer.data)
