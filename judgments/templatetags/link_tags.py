1a2cf2feaab1a28f5cdaf60433c8d8a00bb9bb8e
James Harries <dxw@pancakeuprising.com>
2025-06-13 12:24:28 +0100

    FCL-919 | handle no request

--- judgments/templatetags/link_tags.py
+++ judgments/templatetags/link_tags.py
@@ -14,6 +14,8 @@ def trackable_class_name(text):
 def trackable_link(context, text, **attrs):
     request = context.get("request")
 
+    current_path = request.path if request else ""
+
     class_name = trackable_class_name(text)
 
-    return {"text": text, "attrs": attrs, "class_name": class_name, "current_path": request.path}
+    return {"text": text, "attrs": attrs, "class_name": class_name, "current_path": current_path}
