e621c8d2cd0065333d10ccb26c75d7d483368e04
James Harries <dxw@pancakeuprising.com>
2025-06-19 15:18:27 +0100

    FCL-845 | don't expose internal id on pdf download

--- judgments/views/detail/best_pdf.py
+++ judgments/views/detail/best_pdf.py
@@ -15,10 +15,15 @@ def best_pdf(request, document_uri):
     If there's a DOCX-derived PDF in the S3 bucket, return that.
     Otherwise fall back and redirect to the weasyprint version."""
     pdf = DocumentPdf(document_uri)
+
     response = requests.get(pdf.generate_uri())
     logging.debug("Response %s", response.status_code)
     if response.status_code == 200:
-        return HttpResponse(response.content, content_type="application/pdf")
+        response = HttpResponse(response.content, content_type="application/pdf")
+
+        response["Content-Disposition"] = f"attachment; filename={document_uri}.pdf"
+
+        return response
 
     if response.status_code != 404:
         logging.warn(f"Unexpected {response.status_code} error on {document_uri} whilst trying to get_best_pdf")
