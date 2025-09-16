8f62e592e6470cd599e3e7d32991a545a1171685
James Harries <dxw@pancakeuprising.com>
2025-09-16 21:37:30 +0100

    FCL-1195 | add device info to feedback link

--- ds_judgements_public_ui/static/js/src/feedback_link.js
+++ ds_judgements_public_ui/static/js/src/feedback_link.js
@@ -8,6 +8,25 @@ export const setupFeedbackLink = function () {
     const width = window.innerWidth || document.documentElement.clientWidth;
     const height = window.innerHeight || document.documentElement.clientHeight;
 
+    const uaData = navigator.userAgentData;
+
+    if (uaData && uaData.platform) {
+        url.searchParams.set("device-platform", uaData.platform);
+        url.searchParams.set(
+            "device-class",
+            uaData.mobile ? "mobile" : "desktop",
+        );
+    } else {
+        const deviceClass =
+            width <= 768 ? "mobile" : width <= 1024 ? "tablet" : "desktop";
+
+        url.searchParams.set("device-class", deviceClass);
+
+        if (navigator.platform) {
+            url.searchParams.set("device-platform", navigator.platform);
+        }
+    }
+
     url.searchParams.set("browser-width", String(width));
     url.searchParams.set("browser-height", String(height));
 
