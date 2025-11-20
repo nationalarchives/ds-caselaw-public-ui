e46fb0eded0128d56ab25354e29b86146002335b
James Harries <dxw@pancakeuprising.com>
2025-11-20 15:49:53 +0000

    FCL-1241 | migrate judgment page to jinja

--- judgments/resolvers/document_resolver_engine.py
+++ judgments/resolvers/document_resolver_engine.py
@@ -6,7 +6,7 @@ from django.http.request import HttpRequest
 from django.views.generic import View
 
 from judgments.utils import api_client
-from judgments.views.detail import best_pdf, detail_html, detail_xml, generated_pdf
+from judgments.views.detail import best_pdf, detail_html, detail_jinja, detail_xml, generated_pdf
 
 
 class MultipleResolutionsError(Exception):
@@ -14,19 +14,22 @@ class MultipleResolutionsError(Exception):
 
 
 class DocumentResolverEngine(View):
+    fileformat_lookup = {
+        "data.pdf": best_pdf,
+        "generated.pdf": generated_pdf,
+        "data.xml": detail_xml,
+        "data.html": detail_html,
+    }
+
+    def get_default_renderer(self):
+        return detail_html
+
     def dispatch(
         self,
         request: HttpRequest,
         document_uri: str,
         file_format: Optional[str] = None,
     ):
-        fileformat_lookup = {
-            "data.pdf": best_pdf,
-            "generated.pdf": generated_pdf,
-            "data.xml": detail_xml,
-            "data.html": detail_html,
-        }
-
         resolved_documents = api_client.resolve_from_identifier_slug(DocumentIdentifierSlug(document_uri))
 
         if not resolved_documents:
@@ -41,6 +44,17 @@ class DocumentResolverEngine(View):
         database_uri = resolved_documents[0].document_uri.as_document_uri()
 
         if file_format:
-            return fileformat_lookup[file_format](request, database_uri)
+            return self.fileformat_lookup[file_format](request, database_uri)
 
-        return detail_html(request, database_uri)
+        renderer = self.get_default_renderer()
+        return renderer(request, database_uri)
+
+
+class JinjaDocumentResolverEngine(DocumentResolverEngine):
+    def get_default_renderer(self):
+        return detail_jinja
+
+    fileformat_lookup = {
+        **DocumentResolverEngine.fileformat_lookup,
+        "data.html": detail_jinja,
+    }
