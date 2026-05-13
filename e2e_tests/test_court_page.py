b83a305209ca9ad4f6290fcbc92cd1cbbea12fa5
James Harries <dxw@pancakeuprising.com>
2026-05-13 10:31:02 +0100

    FCLC-1884 | expand static content for single court pages

--- e2e_tests/test_court_page.py
+++ e2e_tests/test_court_page.py
@@ -1,14 +1,56 @@
-from playwright.sync_api import Page, expect
+import pytest
+from playwright.sync_api import Page
 
 from .utils.assertions import assert_is_accessible, assert_matches_snapshot
 
+courts_and_tribunals = [
+    "ewhc/pat",
+    "ewhc/tcc",
+    "ewhc/mercantile",
+    "ewhc/comm",
+    "ewhc/ch",
+    "ewhc/scco",
+    "ewhc/fam",
+    "ewhc/ipec",
+    "ewhc/admin",
+    "ewhc/admlty",
+    "ewhc/kb",
+    "ukpc",
+    "ewca",
+    "eat",
+    "ewcc",
+    "ewca/crim",
+    "ewca/civ",
+    "paac",
+    "ukftt/grc",
+    "ukftt/tc",
+    "ukftt/estate",
+    "ukftt/hesc",
+    "ukftt/credit",
+    "ftt/pc",
+    "ftt/phl",
+    "ftt/transport",
+    "ftt/claims",
+    "ukut/aac",
+    "ukut/iac",
+    "ukut/tcc",
+    "ukut/lc",
+    "ukist",
+    "uksc",
+    "ukiptrib",
+    "ewfc",
+    "ewcop",
+    "ukftt",
+    "ewcr",
+    "siac",
+    "poac",
+    "ewhc",
+]
 
-def test_court_page(page: Page):
-    page.goto("/courts-and-tribunals/ukpc")
 
-    expect(page).to_have_title("Privy Council - Find Case Law - The National Archives")
+@pytest.mark.parametrize("court", courts_and_tribunals)
+def test_court_page(page: Page, court):
+    page.goto(f"/courts-and-tribunals/{court}")
 
-    expect(page.locator("h1:visible")).to_have_text("Privy Council")
-
-    assert_matches_snapshot(page, "court_page")
+    assert_matches_snapshot(page, f"court_{court}_page")
     assert_is_accessible(page)
